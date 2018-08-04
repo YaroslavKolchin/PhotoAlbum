8cdaadb65314d7153a71670de2fc57362eac376f
YaroslavKolchin <39592718+YaroslavKolchin@users.noreply.github.com>
2018-08-04 17:59:43 +0600

    DB

--- src/java/db/DB.java
+++ src/java/db/DB.java
@@ -7,13 +7,15 @@ package db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 
 public class DB {
-  public void dbConnection() throws Exception, SQLException  {
+  public int dbAuthorisation(String email,String pass) throws Exception, SQLException  {
+      int rows=0;
       try
       {        
         String driver = "com.mysql.jdbc.Driver";        
@@ -23,14 +25,40 @@ public class DB {
         Class.forName(driver);
         Connection connection = DriverManager.getConnection(connectionString, user, password);
         Statement stmt = connection.createStatement();
-        String query = "select * from USERS;";
+        String query = "SELECT * FROM USERS WHERE user_email='"+email+"' AND user_password='"+pass+"';";
         ResultSet rs = stmt.executeQuery(query);
+        
         while (rs.next()) {           
-            int userId = rs.getInt("user_id");
-            String userName = rs.getString("user_email");
-            String userPassword = rs.getString("user_password");
-            System.out.println("id: "+userId+"; userName: "+userName+"; userPassword: "+userPassword);
-        }        
+             rows = rs.getInt(1);        
+        } 
+        System.out.println("rows: "+rows);
+        if (!connection.isClosed()) 
+        {      
+            connection.close();
+        }
+        
+      }
+      catch(Exception exception)
+      {
+          exception.printStackTrace();
+      }
+      return rows;
+  }
+  public void dbRegistration(String email,String password) throws Exception, SQLException  {
+      try
+      {        
+        String driver = "com.mysql.jdbc.Driver";        
+        String connectionString = "jdbc:mysql://localhost:3306/AlbumsDB";
+        String user = "albums_admin";
+        String pass = "alBUM_2018";
+        Class.forName(driver);
+        Connection connection = DriverManager.getConnection(connectionString, user, pass);
+        String query = " insert into USERS (user_email, user_password)"
+        + " values (?,?)"; 
+        PreparedStatement preparedStmt = connection.prepareStatement(query);
+        preparedStmt.setString (1, email);
+        preparedStmt.setString (2, password);
+        preparedStmt.execute();
         if (!connection.isClosed()) 
         {
             connection.close();
@@ -41,4 +69,37 @@ public class DB {
           exception.printStackTrace();
       }
   }
+    public void dbAlbumAdd(String name,String description,String owner) throws Exception, SQLException  {
+      try
+      { 
+        java.util.Date dt = new java.util.Date(); 
+        java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String date = sdf.format(dt);
+        int owner_id=1; 
+        String path="/home/PhotoAlbum/User/Email/"+name+"/";
+        String driver = "com.mysql.jdbc.Driver";        
+        String connectionString = "jdbc:mysql://localhost:3306/AlbumsDB";
+        String user = "albums_admin";
+        String pass = "alBUM_2018";
+        Class.forName(driver);
+        Connection connection = DriverManager.getConnection(connectionString, user, pass);
+        String query = " insert into ALBUMS ( album_owner_id,album_name,album_description,album_date_create,album_path)"
+        + " values (?,?,?,?,?)"; 
+        PreparedStatement preparedStmt = connection.prepareStatement(query);
+        preparedStmt.setInt (1, owner_id);
+        preparedStmt.setString (2, name);
+        preparedStmt.setString (3, description);
+        preparedStmt.setString (4, date);
+        preparedStmt.setString (5, path);
+        preparedStmt.execute();
+        if (!connection.isClosed()) 
+        {
+            connection.close();
+        }
+      }
+      catch(Exception exception)
+      {
+          exception.printStackTrace();
+      }
+    }
 }
