7a505b5cd1ab93ff3cbb4c2dde87577acbf90bd8
tp023988 <tp023988@gmail.com>
2019-01-26 03:00:39 +0600

    display photos 3

---
+++ src/java/servlets/PhotoImagesServlet.java
@@ -0,0 +1,131 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author talgat
+ */
+public class PhotoImagesServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet PhotoImagesServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet PhotoImagesServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(true);
+        String albumId = request.getParameter("albumId");
+        String owner="2019";
+        response.setContentType("image/jpg");
+        ServletOutputStream stream = response.getOutputStream();
+        if(session.getAttribute("photo_owner_id")!=null)
+        {
+            owner=session.getAttribute("photo_owner_id").toString(); 
+        }
+        String userDirectory=System.getProperty("user.home");
+        File PhotoDirectory = new File(userDirectory+"/PhotoAlbum/"+owner+"/"+albumId);
+        System.out.println("test 89");           
+        File[] listOfFiles = PhotoDirectory.listFiles();
+        System.out.println("test 90");
+        for (int i = 0; i < listOfFiles.length; i++)
+        {
+            System.out.println("test 91 "+i);
+            if (listOfFiles[i].isFile())
+            {
+                System.out.println("File " + listOfFiles[i].getName());
+                FileInputStream fis = new FileInputStream(listOfFiles[i].getName());
+                BufferedInputStream bin = new BufferedInputStream(fis);  
+                BufferedOutputStream bout = new BufferedOutputStream(stream);  
+                int ch = 0;
+                while((ch = bin.read())!=-1)  
+                {  
+                    bout.write(ch);
+                }
+                bin.close();
+                fis.close();
+                bout.close();
+            } 
+            else if (listOfFiles[i].isDirectory()) 
+            {
+                System.out.println("Directory " + listOfFiles[i].getName());
+            }                
+        }
+        System.out.println("test 90");
+        stream.close();        
+        //processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/servlets/PhotoServlet3.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author talgat
+ */
+public class PhotoServlet3 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet PhotoServlet3</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet PhotoServlet3 at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    private String encoding = "UTF-8";
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        request.setCharacterEncoding(encoding);
+        response.setCharacterEncoding(encoding);
+        int thisServlet = 3;       
+        String albumId = request.getParameter("albumId");
+        String owner = request.getParameter("photo_owner_id"); 
+        ServletOutputStream stream = response.getOutputStream();
+        String userDirectory=System.getProperty("user.home");
+        File PhotoDirectory = new File(userDirectory+"/PhotoAlbum/"+owner+"/"+albumId);
+        System.out.println("thisServlet= "+thisServlet);           
+        File[] listOfFiles = PhotoDirectory.listFiles();
+        if(listOfFiles.length > 2 && listOfFiles.length <= 10)
+        {
+            System.out.println("file path and name" + PhotoDirectory.getAbsolutePath()+"/"+listOfFiles[2].getName());
+            FileInputStream fis = new FileInputStream(PhotoDirectory.getAbsolutePath()+"/"+listOfFiles[2].getName());
+            BufferedInputStream bin = new BufferedInputStream(fis);  
+            BufferedOutputStream bout = new BufferedOutputStream(stream);  
+            int ch = 0;
+            while((ch = bin.read())!=-1)  
+            {  
+                bout.write(ch);
+            }
+            bin.close();
+            fis.close();
+            bout.close();
+        }
+        else if( listOfFiles.length < thisServlet)
+        {           
+            int i = 0;
+            if(listOfFiles.length != 1)       
+            {
+                i = thisServlet%listOfFiles.length;    
+            }
+            System.out.println("File " + PhotoDirectory.getAbsolutePath()+"/"+listOfFiles[i].getName());
+            FileInputStream fis = new FileInputStream(PhotoDirectory.getAbsolutePath()+"/"+listOfFiles[i].getName());
+            BufferedInputStream bin = new BufferedInputStream(fis);  
+            BufferedOutputStream bout = new BufferedOutputStream(stream);  
+            int ch = 0;
+            while((ch = bin.read())!=-1)  
+            {  
+                bout.write(ch);
+            }
+            bin.close();
+            fis.close();
+            bout.close();            
+        }
+        stream.close();    
+        //processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
